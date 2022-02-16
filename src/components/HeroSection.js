b8650f0e226f3ef7d7b5876c2c5fc3324b672b2e
Fede <ferrelli.mazza@gmail.com>
2022-02-15 22:17:19 -0300

    Add bubles tio HeroSection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -19,11 +19,15 @@ function HeroSection() {
                     Web Developer
                 </p>
 
-                <div className='w-20 h-20 rounded-full bg-gradient-to-b from-cyan-500 to-blue-500 border border-white/25 shadow shadow-gray-500 absolute -bottom-10 right-4 -z-0'></div>
-
+                <div className='w-20 h-20 rounded-full bg-gradient-to-b from-violet-500 to-fuchsia-500 border border-white/25 shadow shadow-gray-500 absolute -bottom-10 right-4 -z-0'></div>
+                <div className='w-8 h-8 rounded-full bg-gradient-to-b from-violet-500 to-fuchsia-500 border border-white/25 absolute top-40 right-24 -z-0 shadow-inner-xl shadow-gray-500' ></div>
             </div>
 
             <div className='w-40 h-40 rounded-full bg-gradient-to-b from-violet-500 to-fuchsia-500 border border-white/25 absolute top-1 right-2 -z-0 shadow-inner-xl shadow-gray-500' ></div>
+
+            <div className='w-10 h-10 rounded-full bg-gradient-to-b from-violet-500 to-fuchsia-500 border border-white/25 absolute top-48 right-20 -z-0 shadow-inner-xl shadow-gray-500' ></div>
+            <div className='w-12 h-12 rounded-full bg-gradient-to-b from-violet-500 to-fuchsia-500 border border-white/25 absolute top-56 right-2 -z-0 shadow-inner-xl shadow-gray-500' ></div>
+            
 
             <div className='w-32 h-32 rounded-full bg-gradient-to-r from-purple-500 to-pink-500 border border-white/25 absolute bottom-10 left-10 shadow-xl shadow-[#1f2687]/25' ></div>
 
