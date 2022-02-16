733fddba989d5d20c9f55b038fc0bcb89581a104
Fede <ferrelli.mazza@gmail.com>
2022-02-16 19:09:02 -0300

    Add new hero section design

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,42 +1,36 @@
-import React from 'react'
+import React from 'react';
+import image from '../assets/animacion.gif'
 
 function HeroSection() {
     return (
-        <div className='w-full h-96 flex bg-gradient-to-b bg-violet-800 relative'>
+      <div className="w-full h-screen flex-row justify-evenly bg-violet-900 lg:flex-col overflow-hidden ">
+        <div className="flex flex-row h-80 w-full m-auto mt-24 relative">
+          <img
+            src={image}
+            alt="hero-section"
+            className="  h-[280px] z-20 m-auto"
+          />
 
-            <div className='m-auto w-10/12 h-2/3 p-4 bg-white/25 backdrop-blur-sm shadow-xl shadow-[#1f2687]/25 rounded-xl border-2 border-white/25 z-10
-            
-           
+          <div
+            className="w-40 h-40 rounded-full bg-gradient-to-b from-violet-500 to-fuchsia-500 border border-white/25  shadow-md shadow-gray-600
+          absolute top-1 right-2"
+          ></div>
 
-
-            
-            ' >
-                <h1 className='text-white text-4xl'>
-                    Fede Ferrelli
-                </h1>
-
-                <p className='text-white'>
-                    Web Developer
-                </p>
-
-
-            </div>
-
-            <div className='w-20 h-20 rounded-full bg-gradient-to-b from-violet-500 to-fuchsia-500 border border-white/25 shadow shadow-gray-500 absolute bottom-4 right-4 -z-0'></div>
-            
-            <div className='w-8 h-8 rounded-full bg-gradient-to-b from-violet-500 to-fuchsia-500 border border-white/25 absolute bottom-2 right-32 -z-0 shadow-inner-xl shadow-gray-500' ></div>
-
-            <div className='w-40 h-40 rounded-full bg-gradient-to-b from-violet-500 to-fuchsia-500 border border-white/25 absolute top-1 right-2 -z-0 shadow-inner-xl shadow-gray-500' ></div>
-
-            <div className='w-10 h-10 rounded-full bg-gradient-to-b from-violet-500 to-fuchsia-500 border border-white/25 absolute top-48 right-20 -z-0 shadow-inner-xl shadow-gray-500' ></div>
-            <div className='w-12 h-12 rounded-full bg-gradient-to-b from-violet-500 to-fuchsia-500 border border-white/25 absolute top-56 right-2 -z-0 shadow-inner-xl shadow-gray-500' ></div>
-            
-
-            <div className='w-32 h-32 rounded-full bg-gradient-to-r from-purple-500 to-pink-500 border border-white/25 absolute bottom-10 left-10 shadow-xl shadow-[#1f2687]/25' ></div>
-
-            
+          <div
+            className="w-32 h-32 rounded-full bg-gradient-to-r from-purple-500 to-pink-500 border border-white/25 shadow-md shadow-gray-600
+          absolute top-56 left-10"
+          ></div>
         </div>
-    )
+
+        <div className="text-white text-center mt-16">
+          <h1 className="text-4xl font-bold">Fede Ferrelli</h1>
+          <h2 className="text-lg uppercase ">Front end developer</h2>
+          <button className="bg-violet-400 p-4 uppercase rounded-xl mt-4 text-violet-900 hover:bg-violet-600 hover:text-violet-400">
+            Contactame
+          </button>
+        </div>
+      </div>
+    );
 }
 
 export default HeroSection;
