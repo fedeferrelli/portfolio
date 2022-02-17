f7a53d2edbad144ea8cb22a0eb4b19f04a6f6c36
Fede <ferrelli.mazza@gmail.com>
2022-02-17 19:25:27 -0300

    Add About me page

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function About() {
     return (
-      <div className="h-96 bg-white mt-0 overflow-hidden">
-        <div className=" h-12 bg-gradient-to-b from-purple-800"></div>
+      <div className="bg-white mt-0 overflow-hidden">
+        <div className=" h-4 bg-gradient-to-b from-purple-800 mb-8"></div>
 
-        <div class="text-4xl font-extrabold text-center">
+        <div class="text-5xl font-extrabold text-center">
           <span class="bg-clip-text text-transparent bg-gradient-to-r from-violet-900 via-purple-800 to-pink-500">
             Quién Soy
           </span>
@@ -15,3 +15,4 @@ function About() {
 }
 
 export default About;
+
