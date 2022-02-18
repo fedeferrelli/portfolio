c1d9117347e84a5d677cafba0a036f608a02a2cb
Fede <ferrelli.mazza@gmail.com>
2022-02-18 20:53:14 -0300

    bg-color test 2

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 function About() {
   return (
     <div className="bg-gray-100 mt-0 overflow-hidden mb-8">
-      <div className=" h-4 bg-gradient-to-b from-purple-800 mb-8"></div>
+      <div className=" h-4 bg-gradient-to-b from-purple-800 to-gray-100 mb-8"></div>
 
       <div className="text-5xl font-extrabold text-center">
         <span className="bg-clip-text text-transparent bg-gradient-to-r from-violet-900 via-purple-800 to-pink-500">
